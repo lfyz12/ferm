d2b31c127b0a442981c00af09567fc07e4287ab6
xxxioan <kraskafet@gmail.com>
2025-04-25 17:36:24 +0500

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,19 +1,18 @@
-const {Sequelize} = require('sequelize') // Инициализация секвалайзера
+const { Sequelize } = require('sequelize');
 
-//Подключение к бд
 module.exports = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
         dialect: 'postgres',
-        dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // для Railway
-    },
-  },
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
     }
-)
+);
