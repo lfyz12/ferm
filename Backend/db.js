cafa57c7d2a43c12a643a9b65dd883abdbb12625
xxxioan <kraskafet@gmail.com>
2025-04-25 17:30:22 +0500

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -7,6 +7,12 @@ module.exports = new Sequelize(
     process.env.DB_PASSWORD,
     {
         dialect: 'postgres',
+        dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // для Railway
+    },
+  },
         host: process.env.DB_HOST,
         port: process.env.DB_PORT
     }
